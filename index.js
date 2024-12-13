de0d31ac25444ace1d9e22d07067c9aa89edff04
Khurshedspi <khurshed5322@gmail.com>
2024-12-13 15:15:55 +0600

    get data by email

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ app.get('/jobs/:id', async (req, res) =>{
 // job application apis 
 // get all data, get one data, get some data [0, 1, many]
 
+// get data by email: 
 app.get('/job-application', async(req, res) =>{
   const email = req.query.email;
   const query = {applicant_email: email}
